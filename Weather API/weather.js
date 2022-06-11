e841da53a369922b3d841b7ae210ca527ad0abf2
Sam Kassirov <98607159+kassirov@users.noreply.github.com>
2022-06-11 17:28:58 -0500

    initial comit

---
+++ Weather API/weather.js
@@ -0,0 +1,28 @@
+let weather = {
+  apikey: "01f7d0bec1991e560c4f1ff21ba24bea",
+  fetchWeather: function(city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apikey
+    )
+      .then(response => response.json())
+      .then(data => this.displayWeather(data));
+  },
+  displayWeather: function(data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "wind: " + speed + "km/h";
+  }
+};
